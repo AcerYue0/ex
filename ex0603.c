9df7848970558309cb39820ed8776cbc8b2ac026
AcerYue0 <leeyuchin2618005@gmail.com>
2018-03-23 00:21:03 +0800

    backup
    variable rename
    add description
    output change

--- ex0603.c
+++ ex0603.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 
+const float first_section = 75 * 60;                         //first fee section set
+const float second_section = 75 * 60 + 75 * 1.25 * 15;      //second fee section set
 int main(){
-    int g[3], h[3], i;
+    int i;
+    float fee[3], hour[3];
     for(i = 0; i < 3; i++){
-        scanf("%d", &h[i]);
-        g[i] = h <= 60 ? (75 * h[i]) : (h <= 75 ? (75 * 60 + 75 * 1.25 * (h[i] - 60)) : (75 * 60 + 75 * 1.25 * 15 + 75 * 1.75 * (h[i] - 75)));
+        scanf("%f", &hour[i]);                //for each i, input parking hours
+        fee[i] = hour[i] <= 60 ? (75 * hour[i]) : (
+			     hour[i] <= 75 ? (first_section + 75 * 1.25 * (hour[i] - 60)) :  //in second section, add in fee of first section and get lease section multiplied by 1.25
+		 		  	  		     (second_section + 75 * 1.75 * (hour[i] - 75))); //same as line 11 and new multiplier is going to be 1.75
     }
     for(i = 0; i < 3; i++){
-        printf("%d\n", g[i]);
+        printf("%.0f\n", fee[i]);
     }
     system("pause");
 }
