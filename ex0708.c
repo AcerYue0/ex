923b63055c757cb2782cab30b30735e3a3a1bf2f
AcerYue <33564291+AcerYue0@users.noreply.github.com>
2018-03-31 11:16:06 +0800

    annotation to ex0708

--- ex0708.c
+++ ex0708.c
@@ -1,14 +1,14 @@
-/*ex0708 ASCIIªí
-40618128 §õ¬R¼y*/
+/*ex0708 ASCIIè¡¨
+40618128 ææ˜±æ…¶*/
 #include<stdio.h>
 
 int main(){
     int i, j;
-    printf("     0 1 2 3 4 5 6 7 8 9\n");
+    printf("     0 1 2 3 4 5 6 7 8 9\n");   //typesetting into 10 character per line
     printf("     -------------------\n");
     for(i = 0; i < 26; i++){
-        printf("%2d | ", i);
-        for(j = 0; j < 10; j++){
+        printf("%2d | ", i);                                        //for request output 0~255 of ASCII
+        for(j = 0; j < 10; j++){                                    //but whole table size is 128, above it will output '?'
             if(i * 10 + j > 255) break;
             if(i * 10 + j >= 8 && i * 10 + j <= 13) printf("  ");
             else printf("%c ", (char)(i * 10 + j));
