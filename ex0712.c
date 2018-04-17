88671b7d169ee41017d57d61f75cf43c06cdcac5
AcerYue <33564291+AcerYue0@users.noreply.github.com>
2018-04-18 00:28:31 +0800

    annotation to ex0712 ver.2

--- ex0712.c
+++ ex0712.c
@@ -4,7 +4,7 @@ int main(){
 	double len;
 	int day = 0;
 	scanf("%lf", &len);
-	do{					//in line 7~10
+	do{				//in line 7~10
 		len /= 2;		//each day, length will be halfed
 		day++;			//skip the loop until length is under 5
 	}while(len >= 5);
