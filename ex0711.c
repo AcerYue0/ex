19a91eb0cfca0f9f6cabf11f02d96006911ec5b2
AcerYue <33564291+AcerYue0@users.noreply.github.com>
2018-04-18 00:21:38 +0800

    annotation to ex0711

--- ex0711.c
+++ ex0711.c
@@ -3,21 +3,23 @@
   
 int main(){  
     int input = 1000;  
-    int i, j, t = 0, s = 0;  
+    int i, j, t = 0, s = 1;  
     for(j = 2; j <= input; j++){  
         t = 0;  
-        for(i = j / 2; i >= 1; i--){        //因數和   
-            if(j % i == 0){  
-                t += i;  
+        for(i = j / 2; i >= 1; i--){        //in line 9~13
+            if(j % i == 0){                 //sum all the factors of input expect itself
+                t += i;                     //put in t and reset t every looping
             }  
         }  
-        if(j == t){                         //是否等於原數   
-            if(s != 0)printf(" ");  
+        if(j == t){                         //do when t equals to input
+            if(s != 1)printf(" ");  
             printf("%d", t);  
-            s++;  
-            if(s == 2)j = 100;  
-            if(s == 3)j = 1000;  
-            if(s == 4)j = 100000;  
+            s++;
+            if(s > 1)j = pow(10, s);
+            //according the theorem
+            //preceding few perfect number only appear once every N digits
+            //so in line 18 can skip the loop counting to N+1 digits' first
+            //let looping time be faster
         }  
     }  
     printf("\n");  
