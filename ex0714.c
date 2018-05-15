80ecacd8389a388eb14d286129fc8be03b8253dc
AcerYue0 <leeyuchin2618005@gmail.com>
2018-05-16 00:51:54 +0800

    new homework upload

---
+++ ex0714.c
@@ -0,0 +1,57 @@
+/*ex0714 期中考 
+40618128 李昱慶*/
+#include<stdio.h>
+#include<ctype.h>
+#include<conio.h>
+
+int main(){
+	char input;
+	int i;
+	int bit = 0,               //characters
+	    num = 0,               //numbers
+	    ace = 0,
+	    com = 0,               //punctuation
+	    con = 0,               //controls
+	    els = 0;               //others
+	char ACe[100] = {};
+	do{                               //from line 16~46 each input will be sort out
+		printf("input a test bit:");  //use is___() function to determine which variable it should be
+		input = getche();
+		if(input == 27)break;           //break when input get escape key
+		else if(isdigit(input)){
+			num++;						//the following lines from this line is for numbers
+		}   
+		else if(input == 65 || input == 67 || input == 101){
+	        ACe[ace] = input;
+	        ace++;
+		}   
+		else if(ispunct(input)){
+			com++;   					//punctuations
+		}      
+		else if(iscntrl(input)){
+			con++;   					//controls
+		}      
+		else{
+			els++;  					//and for others
+		}                            
+		bit++;                          //after finishing all determination, go to next bit
+		printf("\n");
+	}while(1);
+	printf("\n");                    
+	printf("numbers:%d\n", num);
+	printf("\n");
+	printf("A, C, e:%d\n", ABC);     
+	printf("A, C, e in the string:");
+	for(i = 0; i < ace; i++){
+		printf("%2c", ACe[i]);
+	}
+	printf("\n");   
+	printf("punctuation:%d\n", com);
+	printf("\n");
+	printf("controls:%d\n", con); 
+	printf("\n");     
+	printf("others:%d\n", els);
+	printf("others:");
+	printf("\n");           
+	printf("total:%d\n", bit);          
+}
---
+++ ex0901.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+
+void score_handler(int classs, int score);
+void A_class(int score);
+void B_class(int score);
+int person_A = 0, person_B = 0;
+int alls_A = 0, alls_B = 0;
+int pass_A = 0, pass_B = 0;
+
+int main(){
+	int i, j, score, s[2][70];
+	for(i = 0; i <= 1; i++){
+		printf("input score of class %s:", i == 0 ? "A" : "B");
+		for(j = 0; j < 70; j++){
+			scanf("%d", &score);
+			if(score == -1)break;
+			else if(score > 100 || score <= -2) printf("error score, please enter again\n");
+			else {
+				if(i == 0)A_class(score);
+				else B_class(score);
+			}
+			s[i][j] = score;
+		}
+	}
+	printf("                              正修科技大學資工系\n");
+	printf("                              計算機程式設計(二)\n");
+	printf("               ------------------------------------------------\n");
+	printf("               班   級|修課|總分|總平均|  及   格  | 不  及  格\n");
+	printf("                      |人數|    |      |人數 百分比|人數 百分比\n");
+	printf("               ================================================\n               ");
+	A_class(101); 
+	printf("\n               ------------------------------------------------\n               ");
+	B_class(101);
+	printf("\n               ------------------------------------------------\n");
+	printf("               合   計|%4d|%4d|%6.2f|%3d, %5.1f%%|%3d, %5.1f%%\n", 
+	person_A + person_B,
+	alls_B + alls_A, 
+	(float)(alls_B + alls_A) / (float)(person_A + person_B),
+	pass_A + pass_B,
+	(float)(pass_B + pass_A) / (float)(person_A + person_B) * 100, 
+	person_A + person_B - (pass_B + pass_A),
+	100 - ((float)(pass_B + pass_A) / (float)(person_A + person_B) * 100));
+}
+void A_class(int score){
+	if(score != 101){
+		alls_A += score;
+		person_A++;
+		if(score >= 60)pass_A++;
+	}
+	else if(score == 101){
+		printf("class A|%4d|%4d|%6.2f|%3d, %5.1f%%|%3d, %5.1f%%", 
+		person_A, 
+		alls_A, 
+		(float)alls_A / (float)person_A, 
+		pass_A,
+		(float)pass_A / (float)person_A * 100, 
+		person_A - pass_A,
+		100 - ((float)pass_A / (float)person_A * 100));
+	}
+}
+void B_class(int score){
+	if(score != 101){
+		alls_B += score;
+		person_B++;
+		if(score >= 60)pass_B++;
+	}
+	else if(score == 101){
+		printf("class B|%4d|%4d|%6.2f|%3d, %5.1f%%|%3d, %5.1f%%", 
+		person_A, 
+		alls_B, 
+		(float)alls_B / (float)person_B, 
+		pass_B,
+		(float)pass_B / (float)person_B * 100, 
+		person_B - pass_B,
+		100 - ((float)pass_B / (float)person_B * 100));
+	}
+}
