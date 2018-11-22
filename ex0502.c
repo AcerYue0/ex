d3fe945605ec3cb30b4ee332a8659a5522c25513
AcerYue <33564291+AcerYue0@users.noreply.github.com>
2018-11-22 21:21:36 +0800

    Update ex0502.c

--- ex0502.c
+++ ex0502.c
@@ -3,12 +3,17 @@
 #include<time.h>
 
 int main(void){
-    int pc;
+    int pc1, pc2;
     char guess;
     srand(time(NULL));
-    pc = (rand() % 3) + 97; //97~99 random
+    pc1 = rand() % 2; //0~1 random
+    pc2 = rand() % 2; //0~1 random
+    int pc = pc1 + pc2; //0~2 final result
     printf("please input your choice --> ");
     scanf("%c", &guess); 
+    //a is for situation 0
+    //b is for situation 1
+    //c is for situation 2
     if(guess == 'a'){
         printf("your guess : 二只均為人像\n");
     }
@@ -18,16 +23,16 @@ int main(void){
     else{
         printf("your guess : 二指均為50元\n");
     }
-    if(pc == 'a'){
+    if(pc == 0){
         printf("pc         : 二只均為人像\n");
     }
-    else if(pc == 'b'){
+    else if(pc == 1){
         printf("pc         : 一只人像一只50元\n");
     }
     else{
         printf("pc         : 二指均為50元\n");
     }
-    if(pc == guess){
+    if(pc + 97 == guess){
         printf("right answer!\n");
     }
     else{
